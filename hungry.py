a47d49d8009ef7d495f2f7411bff9c713a11666c
zafar1162014 <zafarulhaq1162014@gmail.com>
2025-03-13 23:50:58 +0500

    calculation

--- hungry.py
+++ hungry.py
@@ -4,3 +4,9 @@ print("burger")
 x=5
 y=10
 print("Sum of Two Number",x+y)
+print("Sub of Two Number",x-y)
+print("Mul of Two Number",x*y)
+print("Div of Two Number",x/y)
+print("Mod of Two Number",x%y)
+print("Power of Two Number",x**y)
+print("Floor Division of Two Number",x//y)
